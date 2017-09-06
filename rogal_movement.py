1d9629b6a54432b581d6bde111a3210078886bea
Robstepa <zb.arthur@gmail.com>
2017-09-06 11:27:08 +0200

    Add changes in player stats

--- rogal_movement.py
+++ rogal_movement.py
@@ -4,6 +4,7 @@ import tty
 import time
 import termios
 import random
+from stats import display_player_stats
 
 
 def illuminati(filename="illuminati.txt"):
@@ -67,19 +68,21 @@ def print_board(tablica):
             print(tablica[row][column], end='')
 
 
-def switch(tablica, row, column, new_row, new_column, level):
+def switch(tablica, row, column, new_row, new_column, level, player_stats):
     tasks = ['6*8-4*5', '2+2*2', '5+6*7-1*2*3', '2*6+7*9-2*4', '8*15-12*6', '98*72-75*94', '68*23-55*27']
     answers = ['20', '6', '41', '67', '48', '6', '79']
-    task_number = random.randint(0, 7)
+    task_number = random.randint(0, 6)
     if tablica[new_row][new_column] == '#':  # tu warunek, jeśli new_col nie przekracza zakresu i == '#'
         return (row, column, level)
     elif tablica[new_row][new_column] == '%':
         tablica[new_row][new_column] = '.'
-        a = input(tasks[task_number]+": ")
-        if a == answers[task_number]:
-            print("good")
+        answer = input(tasks[task_number]+": ")
+        if answer == answers[task_number]:
+            print("Good")
+            player_stats['Luck'] += 1
         else:
-            print("bad")
+            print("Bad")
+            player_stats['Luck'] -= 1
         return (row, column, level)
     elif tablica[new_row][new_column] == 'O':
         return (row, column, level+1)
@@ -89,24 +92,24 @@ def switch(tablica, row, column, new_row, new_column, level):
         return (new_row, new_column, level)
 
 
-def movement(inp, tablica, row, column, column_len, row_len, level):
+def movement(inp, tablica, row, column, column_len, row_len, level, player_stats):
 
     if inp == 'w':
         if row == 0:
-            return switch(tablica, row, column, row+column_len, column, level)
-        return switch(tablica, row, column, row-1, column, level)
+            return switch(tablica, row, column, row+column_len, column, level, player_stats)
+        return switch(tablica, row, column, row-1, column, level, player_stats)
     elif inp == 's':
         if row == column_len:
-            return switch(tablica, row, column, row-column_len, column, level)
-        return switch(tablica, row, column, row+1, column, level)
+            return switch(tablica, row, column, row-column_len, column, level, player_stats)
+        return switch(tablica, row, column, row+1, column, level, player_stats)
     elif inp == 'd':
         if column == row_len:
-            return switch(tablica, row, column, row, column-row_len, level)
-        return switch(tablica, row, column, row, column+1, level)
+            return switch(tablica, row, column, row, column-row_len, level, player_stats)
+        return switch(tablica, row, column, row, column+1, level, player_stats)
     elif inp == 'a':
         if column == 0:
-            return switch(tablica, row, column, row, column+row_len, level)
-        return switch(tablica, row, column, row, column-1, level)
+            return switch(tablica, row, column, row, column+row_len, level, player_stats)
+        return switch(tablica, row, column, row, column-1, level, player_stats)
 
     return (row, column, level)
 
@@ -116,6 +119,8 @@ def main():
     # illuminati()
     # welcome_screen()
     # how_to_play()
+    player_stats = {'Books': 0, 'Luck': 1, 'Life': 0}
+
     tablica = []
     file_paths = ["map.txt", "map_lvl_2.txt"]
     tablica = make_a_bord(file_paths[0])
@@ -131,10 +136,12 @@ def main():
 
     user_move = ''
     while user_move != 'q':
+        display_player_stats(player_stats)
         user_move = getch()
         os.system('clear')
-        user_position_coordinates = movement(user_move, tablica, user_position_coordinates[0], user_position_coordinates[1], board_len_column, board_len_row, user_position_coordinates[2])
+        user_position_coordinates = movement(user_move, tablica, user_position_coordinates[0], user_position_coordinates[1], board_len_column, board_len_row, user_position_coordinates[2], player_stats)
         if user_position_coordinates[2] == 2:
+            display_player_stats(player_stats)
             tablica = make_a_bord(file_paths[1])
             board_len_column = len(tablica) - 1
             board_len_row = len(tablica[0]) - 2
