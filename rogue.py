190937c388be4fb0cde49244004f1435359631aa
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-09-06 19:42:14 +0200

    add core

--- rogue.py
+++ rogue.py
@@ -1,62 +1,26 @@
-import time
-import sys
-import os
-
-
-def getch():
-    import tty, termios
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
-
-
-def illuminati(filename="illuminati.txt"):
-    art = open(filename)
-    print(art.read())
-    time.sleep(5)
-    art.close()
-    os.system('clear')
-
-
-def welcome_screen(filename="welcome.txt"):
-    text = open(filename)
-    for char in text.read():
-        sys.stdout.write(char)
-        sys.stdout.flush()
-        time.sleep(0.1)
-    text.close
-    os.system('clear')
-
-
-def how_to_play(filename="info.txt"):
-    text = open(filename)
-    for char in text.read():
-        sys.stdout.write(char)
-        sys.stdout.flush()
-        time.sleep(0.1)
-    text.close()
-    os.system('clear')
-
-
-def level1(filename="map.txt"):
-    table = []
-    board = open(filename)
-    for i in board.read():
-        table.append(i)      
-    for i in table:
-        print(i, end='')
+import infoprint
+import firstlvl
+import secondlvl
     
 
 def main():
-    level1()
+    # level1()
     #illuminati()
     #welcome_screen()
     #how_to_play()
+
+    #tu wybór postaci i przekazanie jako arg do funkcji
+    if firstlvl.round():
+        #tu info o następnym lvlu
+        if secondlvl():
+            #tu info o następnym lvlu
+            #if third lvl:
+                #printWin, halloffame itp
+                #return
+                
+    
+        
+
 
 
 if __name__ == '__main__':
