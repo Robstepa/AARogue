0fe197823a7cd59508bf17af44264979a8095964
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-09-07 23:47:59 +0200

    the worst commit name ever

--- rogue.py
+++ rogue.py
@@ -7,6 +7,7 @@ import os
 import hero_creation
 import time
 import halloffame
+import inffooooo
 
 
 def main():
@@ -15,13 +16,14 @@ def main():
     #infoprint.welcome_screen()
     #infoprint.how_to_play()
     # tu wybór postaci i przekazanie jako arg do funkcji
-    file_paths = ["map.txt", "map_lvl_2.txt", "map_lvl_boss.txt"]
+    file_paths = ["map.txt", "map_lvl_2.txt", "map_lvl_boss.txt", "halloffame.csv"]
     player_stats = {'Keys': 0, 'Luck': 1, 'Life': 10, 'Guess': 0, 'Points': 0}
 
-    filename = "halloffame.csv"
     hero = hero_creation.choose_avatar()
     user_name = hero_creation.choose_nickname()
     time_point = time.time()
+
+    inffooooo.i()
 
     check = firstlvl.round(player_stats, file_paths[0], hero)
     if check[0]:
@@ -33,9 +35,9 @@ def main():
             if check[0]:
                 game_over_time = time.time()
                 game_time = (game_over_time - time_point) // 1
-                halloffame.export_score(filename, player_stats, user_name, game_time)
+                halloffame.export_score(file_paths[3], player_stats, user_name, game_time)
                 infoprint.win_screen()
-                halloffame.import_score(filename)
+                halloffame.import_score(file_paths[3])
                 return
 
 
