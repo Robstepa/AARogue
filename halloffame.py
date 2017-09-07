73f179b8f9fdd57e56487a20d7bca95737b06fcb
Robstepa <zb.arthur@gmail.com>
2017-09-07 23:00:55 +0200

    none

--- halloffame.py
+++ halloffame.py
@@ -1,8 +1,5 @@
 def export_score(filename, player_stats, user_name, game_time):
     export = open(filename, 'a')
-    data_to_export = ''
-    for score in player_stats:
-        data_to_export += str(player_stats['Points'])
     export.write(''.join(user_name) + ": " + str(player_stats['Points']) + " points" + "and it took you " + str(game_time) + " sec" + "\n")
     print(''.join(user_name) + ": " + str(player_stats['Points']) + " points" + "and it took you " + str(game_time) + " sec" + "\n")
     export.close()
